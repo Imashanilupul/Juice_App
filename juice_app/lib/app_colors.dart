e33c9488af494b9b4e1225da410ffca99a5c33c2
Imasha Nilupul <imashanilupul@gmail.com>
2024-09-14 18:04:28 +0530

    search app bar

---
+++ juice_app/lib/app_colors.dart
@@ -0,0 +1,7 @@
+import 'dart:ui';
+
+final Color background = Color(0xFFfafafc);
+final Color buttons_col = Color(0xFF003366);
+final Color image_background = Color(0xFFD9D9D9);
+final Color admin_loading_screen = Color(0xFFC99D47);
+final Color confirmation_color = Color(0xFF07B707);
